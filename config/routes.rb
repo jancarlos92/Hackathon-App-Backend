83acde1aac8cac033c5320da9b506c0db8618661
Jancarlos Acevedo <Jancarlos92@me.com>
2019-04-13 17:44:58 -0400

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
       namespace :v1 do
-          resources :users, only: [:index, :create, :edit, :update],
-          resources :practioner, only: [:index, :create],
+          resources :users, only: [:index, :create, :edit, :update]
+
+          resources :practioners, only: [:index, :create]
+
           resources :checkups, only: [:index, :create, :edit, :update]
      end
   end
